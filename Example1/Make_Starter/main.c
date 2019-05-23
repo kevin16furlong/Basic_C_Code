cf4239ad094632c020a9e69eb7c80298464f5319
kevin16furlong <kevin16furlong@gmail.com>
2019-05-23 16:04:38 +0100

    Including Example1 MakeFile for Area of Shapes

---
+++ Example1/Make_Starter/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<math.h>
+
+
+int main( int argc, char *argv[] )  {
+   int aor;
+
+   aor = areaOfRectangle(10,80);
+   printf("Area of Recctangle: %d", aor);
+}
+
+// a program to find the area of different shapes, 
+// using a makefile to compile a group of c and header files together. 
